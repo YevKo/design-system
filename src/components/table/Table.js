66d1ab5fb6d6c6d846fcd7cbaad3df80e125646c
Yevgeniya Kobrina <yevgeniya@exove.fi>
2023-04-29 12:44:05 +0300

    Making table sortable & custom hook

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,28 +1,39 @@
-function Table({ data, config }) {
+import { Fragment } from 'react';
+
+function Table({ data, config, keyFn }) {
+
+    const renderedHeaders = config.map((column) => {
+        if (column.header) {
+            return <Fragment key={column.label}>{column.header()}</Fragment>;
+        }
+
+        return <th className="px-2" key={column.label}>{column.label}</th>;
+    });
+
+    const renderedRows = data.map((rowData) => {
+        const renderedCells = config.map((column) => {
+            return (
+                <td className="p-2" key={column.label}>
+                    {column.render(rowData)}
+                </td>
+            );
+        });
+
+        return (
+            <tr className="border-b" key={keyFn(rowData)}>
+            {renderedCells}
+            </tr>
+        );
+    });
+
 
     return (
         <table className="table-auto border-spacing-2">
             <thead>
-                <tr className="border-b-2">
-                    {
-                        config.map((column) => {
-                            return <th key={column.label} className="p-2">{column.label}</th>
-                        })
-                    }
-                </tr>
+                <tr className="border-b-2">{renderedHeaders}</tr>
             </thead>
             <tbody>
-                { data.map((rawData, index) => {
-                    return (
-                        <tr key={index} className="border-b-2">
-                            {
-                                config.map((column, index) => {
-                                    return <td key={index} className="p-2">{column.render(rawData)}</td>
-                                })
-                            }
-                        </tr>
-                    );
-                })}
+                {renderedRows}
             </tbody>
         </table>
     );
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,24 +1,31 @@
-import Table from '../components/table/Table'
+import SortableTable from '../components/table/SortableTable'
 import users from './users.json';
 
 function TablePage() {
     const config = [
         {
-            "label": "Name",
-            "render": (user) => user.name
+            label: "Name",
+            render: (user) => user.name,
+            sortValue: (user) => user.name
         },
         {
-            "label": "Age",
-            "render": (user) => user.age
+            label: "Age",
+            render: (user) => user.age,
+            sortValue: (user) => user.age
         },
         {
-            "label": "Color",
-            "render": (user) => <div className={ `p-3 m-2 ${user.color}` }></div>
+            label: "Color",
+            render: (user) => <div className={ `p-3 m-2 ${user.color}` }></div>
         }
     ];
+
+    const keyFn = (user) => {
+        return user.name;
+    };
+
     return (
         <div className="TablePage">
-            <Table data={users.users} config={config}/>
+            <SortableTable data={users.users} config={config} keyFn={keyFn}/>
         </div>
     );
 }
